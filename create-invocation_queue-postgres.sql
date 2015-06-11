adab3f2ab0a798f52d91ff8dfea73e81f4babfe8
gunchin <gunchin@programpark.ru>
2015-06-11 13:24:43 +0300

    readme

---
+++ create-invocation_queue-postgres.sql
@@ -0,0 +1,9 @@
+CREATE TABLE invocation_queue
+(
+  id serial PRIMARY KEY,
+  connection_id integer NOT NULL,
+  statement_id integer NOT NULL,
+  class integer NOT NULL,
+  method_name character varying(23) NOT NULL,
+  args bytea NOT NULL
+)
